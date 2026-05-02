25ce7f645179ae36479ddd20b93407a7a1edd0c8
Yusuf Ali Tangöz <tangozyusufali@gmail.com>
2026-05-02 22:12:36 +0300

    feat: init app

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <Button>Merhaba</Button>
+  </section>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,67 @@
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "nuxt-security",
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/i18n",
+    "@vee-validate/nuxt",
+  ],
+  compatibilityDate: "2026-05-02",
+  devtools: { enabled: false },
+  ssr: false,
+  i18n: {
+    defaultLocale: "tr",
+    locales: [
+      {
+        language: "tr-TR",
+        name: "Türkçe",
+        file: "tr.ts",
+        code: "tr",
+      },
+    ],
+  },
+  vite: {
+    clearScreen: false,
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      strictPort: true,
+    },
+  },
+  fonts: {
+    families: [
+      {
+        weights: [400, 500, 600],
+        name: "Inter",
+        global: true,
+      },
+    ],
+  },
+  security: {
+    headers: {
+      contentSecurityPolicy: false,
+    },
+  },
+  experimental: {
+    viteEnvironmentApi: true,
+  },
+  ignore: ["**/src-tauri/**"],
+  veeValidate: {
+    componentNames: {
+      Field: "VeeField",
+    },
+  },
+  typescript: {
+    typeCheck: true,
+  },
+  shadcn: {
+    prefix: "",
+  },
+  icon: {
+    mode: "svg",
+  },
+});
