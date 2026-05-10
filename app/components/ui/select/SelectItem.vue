7b44d0e87ef09e2292cfbb1e77a96d586b150676
Yusuf Ali Tangöz <tangozyusufali@gmail.com>
2026-05-10 19:31:25 +0300

    fix(select-item): replace Check component with Icon for indicator

--- app/components/ui/select/SelectItem.vue
+++ app/components/ui/select/SelectItem.vue
@@ -32,7 +32,7 @@ const forwardedProps = useForwardProps(delegatedProps);
     <span class="absolute right-2 flex size-3.5 items-center justify-center">
       <SelectItemIndicator>
         <slot name="indicator-icon">
-          <Check class="size-4" />
+          <Icon name="lucide:check" class="size-4" />
         </slot>
       </SelectItemIndicator>
     </span>
