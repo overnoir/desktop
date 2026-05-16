4a867888902449cff0658caf8fc0695df4f086c4
Yusuf Ali Tangöz <tangozyusufali@gmail.com>
2026-05-16 06:48:20 +0300

    feat(ui): progress and spinner

---
+++ app/components/ui/spinner/Spinner.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <Icon
+    name="lucide:loader-circle"
+    role="status"
+    aria-label="Loading"
+    :class="cn('size-4 animate-spin', props.class)"
+  />
+</template>
