01f86538ec6f36877567a1b75b9c796e13ce0c76
Yusuf Ali Tangöz <tangozyusufali@gmail.com>
2026-05-28 13:01:15 +0300

    fix: add missing plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -50,6 +50,7 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_keyring::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_pinia::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_os::init())
         .setup(|app| {
