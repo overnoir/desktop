bbd1a51fcd28e132b268035b73e7f63671e28dbf
Yusuf Ali Tangöz <tangozyusufali@gmail.com>
2026-07-02 22:21:21 +0300

    style(offline): update icon size

--- app/components/overlay/Offline.vue
+++ app/components/overlay/Offline.vue
@@ -1,7 +1,7 @@
 <template>
   <OverlayItem>
     <OverlayItemContent class="bg-red-500! border-red-600">
-      <Icon name="lucide:wifi-off" class="size-[35%]" />
+      <Icon name="lucide:wifi-off" class="size-[50%]" />
     </OverlayItemContent>
   </OverlayItem>
 </template>
