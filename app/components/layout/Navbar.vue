0bb38e1cb7b8acc673e6325822743ac0d7faec21
Yusuf Ali Tangöz <tangozyusufali@gmail.com>
2026-07-04 23:46:47 +0300

    style: update navbar

--- app/components/layout/Navbar.vue
+++ app/components/layout/Navbar.vue
@@ -4,42 +4,51 @@ const { general, connectors } = storeToRefs(useLinkGroupsStore());
 
 <template>
   <nav class="w-50 p-4 pt-0 shrink-0 flex flex-col space-y-4">
-    <ul v-for="(group, i) in [general, connectors]" :key="i" class="space-y-0">
-      <h1 class="text-[.7rem] font-medium mb-1">
+    <ul
+      v-for="(group, i) in [general, connectors]"
+      :key="i"
+      class="space-y-0.5"
+    >
+      <h1 class="text-[.7rem] font-medium">
         {{ group.name }}
       </h1>
-      <li v-for="({ name, to, icon, links }, j) in group.links" :key="j">
-        <div>
-          <Button
-            class="w-full justify-start"
-            size="default"
-            :variant="
-              $route.path === to || links?.some(({ to }) => $route.path === to)
-                ? 'secondary'
-                : 'ghost'
-            "
-            as-child
-          >
-            <NuxtLink :to>
-              <Icon v-if="icon" :name="icon" class="size-4.5 mr-1" />
-              {{ name }}
-            </NuxtLink>
-          </Button>
-          <ul v-if="links" class="ml-5 border-l">
-            <li v-for="(link, k) in links" :key="k" class="ml-2.5">
-              <Button
-                class="w-full text-secondary-foreground h-7 justify-start font-normal"
-                variant="ghost"
-                size="sm"
-                as-child
-              >
-                <NuxtLink :to="link.to">
-                  {{ link.name }}
-                </NuxtLink>
-              </Button>
-            </li>
-          </ul>
-        </div>
+      <li
+        v-for="({ name, to, icon, links }, j) in group.links"
+        :key="j"
+        class="space-y-0.5"
+      >
+        <Button
+          :variant="$route.path === to && !links ? 'outline' : 'ghost'"
+          :class="{
+            'border-0 ring ring-inset ring-border':
+              $route.path === to && !links,
+          }"
+          class="w-full justify-start"
+          size="default"
+          as-child
+        >
+          <NuxtLink :to>
+            <Icon v-if="icon" :name="icon" class="size-4.5 mr-1" />
+            {{ name }}
+          </NuxtLink>
+        </Button>
+        <ul v-if="links" class="ml-5.25 border-l space-y-0.5">
+          <li v-for="(link, k) in links" :key="k" class="ml-2.5">
+            <Button
+              :class="{
+                'border-0 ring ring-inset ring-border': $route.path === link.to,
+              }"
+              :variant="$route.path === link.to ? 'outline' : 'ghost'"
+              class="w-full text-[.8rem] justify-start"
+              size="sm"
+              as-child
+            >
+              <NuxtLink :to="link.to">
+                {{ link.name }}
+              </NuxtLink>
+            </Button>
+          </li>
+        </ul>
       </li>
     </ul>
   </nav>
